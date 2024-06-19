883d494d75c2b80a1e9bc0021730c2321bf86c17
DegreeJr <93307748+DegreeJr@users.noreply.github.com>
2024-06-19 10:53:00 +0700

    Update tubesAlpro.go

--- tubesAlpro.go
+++ tubesAlpro.go
@@ -9,7 +9,9 @@ type tanggal struct {
 }
 
 type dataPasien struct {
-	nama                    string
+	hemoglobinA1c           float64
+	paket                   string
+	namaDepan, namaAkhir    string
 	umur                    int
 	gender                  string
 	TL                      tanggal
@@ -49,20 +51,21 @@ func main() {
 		} else if pilihan == 2 {
 			fmt.Println("Pilih data berikut:")
 			fmt.Println("1. Tampilkan semua data")
-			fmt.Println("2. Tampilkan berdasarkan kategori")
+			fmt.Println("2. Tampilkan berdasarkan kategori yang sama")
 			fmt.Println("3. Tampilkan data dari yang terkini")
 			fmt.Scan(&pilihan_2)
 			if pilihan_2 == 1 {
-				printData(A, n)
+				printSemuaTerurut(A, n)
 			} else if pilihan_2 == 2 {
 				fmt.Println()
 				fmt.Println("Pilih Kategori Data:")
 				fmt.Println("1. Umur")
 				fmt.Println("2. gender")
 				fmt.Println("3. Tahun berkunjung")
+				fmt.Println("4. Nama")
 				fmt.Print("Masukkan angka: ")
 				fmt.Scan(&category)
-				for category != 1 && category != 2 && category != 3 {
+				for category != 1 && category != 2 && category != 3 && category != 4 {
 					fmt.Println("Masukan tidak valid")
 					fmt.Scan(&category)
 				}
@@ -72,6 +75,8 @@ func main() {
 					printDataBerdasarkanGender(A, n)
 				} else if category == 3 {
 					printDataBerdasarkanTahun(A, n)
+				} else if category == 4 {
+					printDataBerdasarkanNama(A, n)
 				}
 			} else if pilihan_2 == 3 {
 				sortingTerbaru(A, n)
@@ -97,8 +102,9 @@ func main() {
 }
 
 func cekKesehatan(A *arrayPasien, n int) {
-	fmt.Println("Masukkan nama lengkap:")
-	fmt.Scan(&A[0].nama)
+	var pilihPaket int
+	fmt.Println("Masukkan nama depan dan belakang:")
+	fmt.Scan(&A[n].namaDepan, &A[n].namaAkhir)
 
 	fmt.Println("Masukkan gender(Male/Female):")
 	fmt.Scan(&A[n].gender)
@@ -130,12 +136,27 @@ func cekKesehatan(A *arrayPasien, n int) {
 	fmt.Print("Tinggi badan (m): ")
 	fmt.Scan(&A[n].tinggiBadan)
 
-	fmt.Println("Masukkan tekanan darah sistolik dan diastolik:")
-	fmt.Print("Tekanan darah Sistolik (mmHg): ")
-	fmt.Scan(&A[n].TDS)
-	fmt.Print("Tekanan darah diastolik (mmHg): ")
-	fmt.Scan(&A[n].TDD)
+	fmt.Println("Masukkan Paket Medical Check Up:")
+	fmt.Println("1. Paket Cek Tekanan Darah")
+	fmt.Println("2. Paket cek Gula Darah")
+	fmt.Print("Masukkan angka 1 atau 2: ")
+	fmt.Scan(&pilihPaket)
+	if pilihPaket == 1 {
+		A[n].paket = "Cek Tekanan Darah"
+		fmt.Println("Masukkan tekanan darah sistolik dan diastolik:")
+		fmt.Print("Tekanan darah Sistolik (mmHg): ")
+		fmt.Scan(&A[n].TDS)
+		fmt.Print("Tekanan darah diastolik (mmHg): ")
+		fmt.Scan(&A[n].TDD)
+		A[n].hemoglobinA1c = 0.0
 
+	} else if pilihPaket == 2 {
+		A[n].paket = "Cek Gula Darah"
+		fmt.Println("Masukkan angka Hemoglobin A1c: ")
+		fmt.Scan(&A[n].hemoglobinA1c)
+		A[n].TDS = 0
+		A[n].TDD = 0
+	}
 	A[n].umur = hitungUmur(A[n].TL.tanggal, A[n].TL.bulan, A[n].TL.tahun, A[n].kunjungan.tanggal, A[n].kunjungan.bulan, A[n].kunjungan.tahun)
 }
 
@@ -167,7 +188,8 @@ func printData(A arrayPasien, n int) {
 		fmt.Println()
 		for i := 0; i < n; i++ {
 			fmt.Println("-----------------------------------------------------")
-			fmt.Println("Nama:", A[i].nama)
+			fmt.Println("Nama:", A[i].namaDepan, A[i].namaAkhir)
+			fmt.Println("Paket:", A[i].paket)
 			fmt.Println("-----------------------------------------------------")
 			fmt.Println("Umur:", A[i].umur)
 			fmt.Println("Gender:", A[i].gender)
@@ -232,6 +254,55 @@ func printDataBerdasarkanTahun(A arrayPasien, n int) {
 	printData(D, m)
 }
 
+func printDataBerdasarkanNama(A arrayPasien, n int) {
+	var D arrayPasien
+	var m int
+	m = 0
+	var namaDepan, namaAkhir string
+	fmt.Println("Masukkan Tahun:")
+	fmt.Scan(&namaDepan, &namaAkhir)
+	for i := 0; i < n; i++ {
+		if A[i].namaDepan == namaDepan && A[i].namaAkhir == namaAkhir {
+			D[m] = A[i]
+			m++
+		}
+	}
+	printData(D, m)
+}
+
+func printSemuaTerurut(A arrayPasien, n int) {
+	var pilihan, pilihanSorting int
+	fmt.Println("Tampilkan terurut berdasarkan:")
+	fmt.Println("1. Usia")
+	fmt.Println("2. Tanggal Medical Checkup")
+	fmt.Println("Masukkan angka: ")
+	fmt.Scan(&pilihan)
+	if pilihan == 1 {
+		fmt.Println("Menaik Atau menurun?")
+		fmt.Println("1. Menaik")
+		fmt.Println("2. Menurun")
+		fmt.Print("Masukkan angka: ")
+		fmt.Scan(&pilihanSorting)
+		if pilihanSorting == 1 {
+			ascendingUsiaInsertion(A, n)
+		} else if pilihanSorting == 2 {
+			descendingUsiaSelecton(A, n)
+		}
+	} else if pilihan == 2 {
+		fmt.Println("Menaik Atau menurun?")
+		fmt.Println("1. Menaik")
+		fmt.Println("2. Menurun")
+		fmt.Print("Masukkan angka: ")
+		fmt.Scan(&pilihanSorting)
+		if pilihanSorting == 1 {
+			ascendingTanggalInsertion(A, n)
+		} else if pilihanSorting == 2 {
+			descendingTanggalSelecton(A, n)
+		}
+	}
+
+}
+
 func sortingTerbaru(A arrayPasien, n int) {
 	var E arrayPasien
 	var m int
@@ -244,18 +315,18 @@ func sortingTerbaru(A arrayPasien, n int) {
 }
 
 func EditData(A *arrayPasien, n int) {
-	var nama string
+	var namaDepan, namaAkhir string
 	fmt.Println("Masukkan nama lengkap pasien yang ingin diedit:")
-	fmt.Scan(&nama)
+	fmt.Scan(&namaDepan, &namaAkhir)
 
 	found := false // Variabel flag untuk menandai apakah nama ditemukan atau tidak
 
 	for i := 0; i < n; i++ {
-		if A[i].nama == nama {
+		if (A[i].namaDepan == namaDepan) && (A[i].namaAkhir == namaAkhir) {
 			// Menampilkan dan memperbarui data pasien
 			found = true
 			fmt.Println("Data pasien yang akan diedit:")
-			fmt.Println("Nama:", A[i].nama)
+			fmt.Println("Nama:", A[i].namaDepan, A[i].namaAkhir)
 			fmt.Println("Umur:", A[i].umur)
 			fmt.Println("Gender:", A[i].gender)
 			fmt.Println("Tanggal Lahir:", A[i].TL.tanggal, "/", A[i].TL.bulan, "/", A[i].TL.tahun)
@@ -278,7 +349,7 @@ func EditData(A *arrayPasien, n int) {
 
 			if choice == 1 {
 				fmt.Println("Masukkan nama baru (terdiri dari nama depan dan nama belakang):")
-				fmt.Scan(&A[i].nama)
+				fmt.Scan(&A[i].namaDepan, &A[i].namaAkhir)
 				fmt.Println("Data berhasil diupdate!")
 			} else if choice == 2 {
 				fmt.Println("Masukkan gender baru:")
@@ -310,16 +381,15 @@ func EditData(A *arrayPasien, n int) {
 		fmt.Println("Pasien tidak ditemukan.")
 	}
 }
-
 func DeleteData(A *arrayPasien, n *int) {
-	var nama string
+	var namaDepan, namaAkhir string
 	fmt.Println("Masukkan nama lengkao pasien yang ingin dihapus:")
-	fmt.Scan(&nama)
+	fmt.Scan(&namaDepan, &namaAkhir)
 
 	found := false // Variabel flag untuk menandai apakah ID ditemukan atau tidak
 
 	for i := 0; i < *n; i++ {
-		if A[i].nama == nama {
+		if A[i].namaDepan == namaDepan && A[i].namaAkhir == namaAkhir {
 			found = true
 			// Geser data ke kiri untuk menutup celah
 			for j := i; j < *n-1; j++ {
@@ -334,4 +404,85 @@ func DeleteData(A *arrayPasien, n *int) {
 	if !found {
 		fmt.Println("nama pasien tidak ditemukan.")
 	}
+}
+
+func ascendingUsiaInsertion(A arrayPasien, k int) {
+	n := len(A)
+	for i := 1; i < n; i++ {
+		key := A[i]
+		j := i - 1
+		for j >= 0 && A[j].umur > key.umur {
+			A[j+1] = A[j]
+			j = j - 1
+		}
+		A[j+1] = key
+	}
+	printData(A, k)
+}
+
+func descendingUsiaSelecton(A arrayPasien, k int) {
+	n := len(A)
+	for i := 0; i < n-1; i++ {
+		minIndex := i
+		for j := i + 1; j < n; j++ {
+			if A[j].umur < A[minIndex].umur {
+				minIndex = j
+			}
+		}
+		A[i], A[minIndex] = A[minIndex], A[i]
+	}
+	printData(A, k)
+}
+
+func ascendingTanggalInsertion(A arrayPasien, k int) {
+	n := len(A)
+	for i := 1; i < n; i++ {
+		key := A[i]
+		j := i - 1
+
+		// Pindahkan elemen arr[0..i-1], yang lebih besar dari key,
+		// ke satu posisi di depan posisi sekarang
+		for j >= 0 && kurangDari(key.kunjungan, A[j].kunjungan) {
+			A[j+1] = A[j]
+			j = j - 1
+		}
+		A[j+1] = key
+	}
+	printData(A, k)
+}
+
+func descendingTanggalSelecton(A arrayPasien, k int) {
+	n := len(A)
+	for i := 0; i < n-1; i++ {
+		// Temukan elemen maksimum dalam array yang belum diurutkan
+		maxIdx := i
+		for j := i + 1; j < n; j++ {
+			if lebihDari(A[j].kunjungan, A[maxIdx].kunjungan) {
+				maxIdx = j
+			}
+		}
+		// Tukar elemen maksimum yang ditemukan dengan elemen pertama
+		A[i], A[maxIdx] = A[maxIdx], A[i]
+	}
+	printData(A, k)
+}
+
+func lebihDari(a, b tanggal) bool {
+	if a.tahun != b.tahun {
+		return a.tahun > b.tahun
+	}
+	if a.bulan != b.bulan {
+		return a.bulan > b.bulan
+	}
+	return a.tanggal > b.tanggal
+}
+
+func kurangDari(a, b tanggal) bool {
+	if a.tahun != b.tahun {
+		return a.tahun < b.tahun
+	}
+	if a.bulan != b.bulan {
+		return a.bulan < b.bulan
+	}
+	return a.tanggal < b.tanggal
 }
